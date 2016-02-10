8d8827e7bddfad8f3515a4d31732ab5834cbfce3
Riley Bracken <rileybracken@gmail.com>
2016-02-10 16:22:51 -0700

    Styles the login component.

--- app/containers/LoginContainer.js
+++ app/containers/LoginContainer.js
@@ -6,11 +6,18 @@ import React, {
   Component,
   StyleSheet,
   PropTypes,
+  Dimensions,
+  Image,
   TouchableHighlight
 } from 'react-native';
 
 import simpleAuthClient from 'react-native-simple-auth';
 import secrets from '../../secrets';
+
+const {
+  width: deviceWidth,
+  height: deviceHeight
+} = Dimensions.get('window');
 
 class Login extends Component {
   static propTypes = {};
@@ -28,11 +35,25 @@ class Login extends Component {
   render() {
     return (
       <View style={ styles.container }>
-        <TouchableHighlight
-          onPress={ () => this.login() }
-        >
-          <Text>Twitter</Text>
-        </TouchableHighlight>
+        <View style={ styles.badge }>
+          <Image source={ require('image!badge') } />
+        </View>
+
+        <View style={ styles.btnContainer }>
+          <TouchableHighlight
+            style={ styles.btn }
+            onPress={ () => this.login() }
+          >
+            <Text style={ styles.btnTxt }>FACEBOOK</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            style={ styles.btn }
+            onPress={ () => this.login() }
+          >
+            <Text style={ styles.btnTxt }>TWITTER</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
@@ -43,7 +64,37 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    position: 'relative',
+    backgroundColor: '#e6e6e6',
+  },
+
+  badge: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  btnContainer: {
+    paddingBottom: 22,
+    paddingHorizontal: 30,
+    width: deviceWidth,
+  },
+
+  btn: {
+    marginBottom: 8,
+    backgroundColor: '#000',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 25,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 20 },
+    shadowOpacity: 0.35,
+    shadowRadius: 13,
+  },
+
+  btnTxt: {
+    color: '#fff',
+    fontFamily: 'Futura-Medium',
   },
 });
 
