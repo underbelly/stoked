d0dd6f7b51ed3444a3c2e0f8027d20a5c2ea74de
Riley Bracken <rileybracken@gmail.com>
2016-03-08 00:15:26 -0700

    Adds openGL support

--- android/app/src/main/java/com/stoked/MainActivity.java
+++ android/app/src/main/java/com/stoked/MainActivity.java
@@ -1,6 +1,7 @@
 package com.stoked;
 
 import com.facebook.react.ReactActivity;
+import com.projectseptember.RNGL.RNGLPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -34,7 +35,8 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-        new MainReactPackage()
+        new MainReactPackage(),
+        new RNGLPackage()
       );
     }
 }
